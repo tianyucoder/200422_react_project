43d94f39dc53d07d9429b7c38baef3458953e88f
tianyu5177 <zhangtianyu5177@sina.com>
2020-09-09 11:29:26 +0800

    更新

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,6 @@
 //路由配置（仅是基本路由的配置，具体用户登录后能用哪些路由，是动态生成的）
 import Login from "@/pages/Login";
 import NotFound from "@/pages/404";
-import Oauth from '@/pages/Oauth'
 
 //常量路由(无需登录即可访问的组件)
 export const constantRoutes = [
@@ -10,7 +9,6 @@ export const constantRoutes = [
 		component: Login,
 		title: "登录",
 	},
-	{ path: "/oauth", component: Oauth },
 	{ path: "*", component: NotFound },
 ];
 
